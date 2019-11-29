5becaa9fa69315c541f73a32e547ec733e5cf061
liuwei <liuwei@itcast.cn>
2019-11-29 12:01:28 +0800

    10-文章列表之分页目录完成

--- src/views/postList.vue
+++ src/views/postList.vue
@@ -9,9 +9,9 @@
     <!-- 表格 -->
     <el-table border :data="postList" style="width: 100%;margin-top:20px">
       <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column prop="title" label="标题" width="360"></el-table-column>
-      <el-table-column prop="create_date" label="时间" width="280"></el-table-column>
-      <el-table-column prop="type" label="类型"></el-table-column>
+      <el-table-column prop="title" label="标题" width="470"></el-table-column>
+      <el-table-column prop="create_date" label="时间" width="200"></el-table-column>
+      <el-table-column prop="type" label="类型" width="50"></el-table-column>
       <el-table-column prop="user.nickname" label="作者" width="120"></el-table-column>
       <el-table-column label="操作" width="230">
         <template slot-scope="scope">
@@ -27,6 +27,16 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页组件 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageIndex"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
   </div>
 </template>
 
@@ -37,21 +47,39 @@ export default {
     return {
       postList: [],
       pageIndex: 1,
-      pageSize: 5
+      pageSize: 2
     }
   },
   methods: {
     edit (row) {
       console.log(row)
+    },
+    // 切换每页显示记录数时触发
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+      // 重置cageSize
+      this.pageSize = val
+      this.init()
+    },
+    // 切换当前页码时触发
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+      this.pageIndex = val
+      this.init()
+    },
+    // 数据初始化
+    async init () {
+      let res = await getPostList({
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      })
+      console.log(res)
+      this.postList = res.data.data
+      this.total = res.data.total
     }
   },
   async mounted () {
-    let res = await getPostList({
-      pageIndex: this.pageIndex,
-      pageSize: this.pageSize
-    })
-    console.log(res)
-    this.postList = res.data.data
+    this.init()
   }
 }
 </script>
